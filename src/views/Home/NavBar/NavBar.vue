98e479b0dbd90c1c3272d9989ccf0788213facdf
Midorisy <837239572@qq.com>
2026-03-03 16:07:41 +0800

    初版nav-bar

---
+++ src/views/Home/NavBar/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="nav-bar">
+        <div class="title">
+            黑马旅途
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.nav-bar {
+    width: 100%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+
+    .title {
+        color: var(--primary-color);
+        font-weight: 600;
+    }
+}
+</style>
