e35a031a518f843372c8baa3c964443b19ebb2dd
Midorisy <837239572@qq.com>
2026-03-03 16:07:29 +0800

    初版homebanner

---
+++ src/views/Home/Location/HomeBanner.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-banner">
+        <img src="@/assets/img/home/banner.webp" alt="" srcset="">
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.home-banner {
+    img {
+        width: 100%;
+    }
+}
+</style>
