52aa11f564a00165735a689e14e14eabade23e8b
Midorisy <837239572@qq.com>
2026-03-03 15:51:03 +0800

    引入vant包

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import 'normalize.css'
+import './assets/css/index.css'
 import App from './App.vue'
 import router from './router'
+import 'vant/lib/index.css'
 
 const app = createApp(App)
 
