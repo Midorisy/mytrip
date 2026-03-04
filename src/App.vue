f504000510cfec546cdd9734dadd51fb59e565ca
Midorisy <837239572@qq.com>
2026-03-05 01:37:42 +0800

    增加通过路由meta隐藏tabbar功能

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
 import TabBar from '@/components/TabBar/index.vue'
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+// watch(route, (newVal, oldVal) => {
+//   console.log('每次进入新页面都会变化');
+// })
+console.log('每次进入新页面都会变化')
 </script>
 
 <template>
   <div class="main">
     <router-view></router-view>
-    <tab-bar></tab-bar>
+    <tab-bar v-if="!route.meta.hideTabbar"></tab-bar>
   </div>
 </template>
 
