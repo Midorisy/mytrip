21fa87c7c541ea4e5721d3694062d99fbe24cf3f
Midorisy <837239572@qq.com>
2026-03-05 01:06:26 +0800

    初步封装了两个axios实例

---
+++ src/server/request.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+import { generateFasTraceId } from "@/apis/createTujiaUid"
+
+export const instanceForTencentMap = axios.create({
+    baseURL:import.meta.env.VITE_API_TENCENTMAP,
+    timeout:10000
+})
+
+export const instance = axios.create({
+    baseURL:import.meta.env.VITE_API_BASE_URL,
+    timeout:10000
+})
+
+instance.interceptors.request.use((config) => {
+
+    // 如果config.params不存在
+    if (!config.params) {
+        config.params = {}
+    } 
+    Object.assign(config.params,generateFasTraceId())
+
+    return config
+})
