a72476b509c5cf18bd0dd3c72c6b8503b1864dcb
Midorisy <837239572@qq.com>
2026-03-06 23:28:48 +0800

    feat: 新增city页面

---
+++ src/views/City/City.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="city">
+        <search-city></search-city>
+        <keep-alive>
+            <city-tab></city-tab>
+        </keep-alive>
+    </div>
+</template>
+
+<script setup>
+import SearchCity from '@/components/SearchCity/SearchCity.vue';
+import CityTab from '@/components/CityTab/CityTab.vue';
+</script>
+
+<style scoped></style>
