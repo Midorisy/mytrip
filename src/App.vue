f195161a7f8e7874399548cae65f02e531b76e7a
Midorisy <837239572@qq.com>
2026-03-06 23:32:11 +0800

    style: 移除多余代码

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
 import TabBar from '@/components/TabBar/index.vue'
-import { watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
-// watch(route, (newVal, oldVal) => {
-//   console.log('每次进入新页面都会变化');
-// })
-console.log('每次进入新页面都会变化')
 </script>
 
 <template>
