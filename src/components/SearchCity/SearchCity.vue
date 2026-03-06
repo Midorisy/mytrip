003a3ab910f0408e2d72f781b26bee941138a92e
Midorisy <837239572@qq.com>
2026-03-06 23:31:36 +0800

    feat: 新增搜索城市组件

---
+++ src/components/SearchCity/SearchCity.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="search-city">
+        <Search class="search-inner" placeholder="城市/区域/位置" show-action @cancel="toHomePage" @onSearch="onSearch"
+            shape="round">
+
+        </Search>
+    </div>
+</template>
+
+<script setup>
+import { Search } from 'vant';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+function onSearch(params) {
+
+}
+/**
+ * 点击取消按钮返回首页
+ */
+function toHomePage() {
+    router.push({
+        path: '/home'
+    })
+}
+</script>
+
+<style scoped>
+.search-city {
+    --van-search-left-icon-color: var(--primary-color);
+    --van-search-input-height: 28px;
+    --van-search-padding: 8px 20px;
+    border-bottom: 1px solid #eee;
+
+    :deep(input[class="van-field__control"]::placeholder) {
+        color: #999;
+    }
+}
+</style>
