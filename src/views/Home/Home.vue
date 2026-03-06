3d948bc99c2d0d91c27995c4871cb7cd13d88b69
Midorisy <837239572@qq.com>
2026-03-06 23:28:25 +0800

    feat(home组件): home组件新增各类组件

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="home">
-        home
+        <nav-bar></nav-bar>
+        <home-banner></home-banner>
+        <home-location></home-location>
     </div>
 </template>
 
 <script setup>
-
+import NavBar from './NavBar/NavBar.vue';
+import HomeBanner from './HomeBanner/HomeBanner.vue';
+import HomeLocation from './HomeLocation/HomeLocation.vue';
 </script>
 
 <style scoped></style>
