11fe16e67e3e9581166d27fda880db5623b098ec
Midorisy <837239572@qq.com>
2026-03-08 15:30:52 +0800

    feat: 新增选定日期、确认键、热门tag的静态

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,13 +3,47 @@
         <nav-bar></nav-bar>
         <home-banner></home-banner>
         <home-location></home-location>
+        <order-collection></order-collection>
+        <hot-tags></hot-tags>
+            <div class="home-search">
+                <Button 
+                type="large"
+                round 
+                block
+                class="search-button"
+                >开始搜索</Button>
+                <span class="text">民宿预定服务由黑马网提供</span>
+            </div>
     </div>
 </template>
 
 <script setup>
+import { Button } from 'vant';
 import NavBar from './NavBar/NavBar.vue';
 import HomeBanner from './HomeBanner/HomeBanner.vue';
 import HomeLocation from './HomeLocation/HomeLocation.vue';
+import OrderCollection from './OrderCollection/OrderCollection.vue';
+import HotTags from './Hot-Tags/HotTags.vue';
 </script>
 
-<style scoped></style>
+<style scoped>
+.home{
+.home-search {
+    text-align: center;
+    padding: 0 20px;
+
+    .search-button{
+        border: none;
+        background-image: linear-gradient(90deg, #fa8c1d, #fcaf3f);
+        font-size: 18px;
+        font-weight: 400;
+        color: #fff;
+    }
+    .text {
+        font-size:10px;
+        color: #999;
+    }
+}
+}
+
+</style>
