747effd994532f827d0fe6e8270083df27c784ec
Midorisy <837239572@qq.com>
2026-03-13 02:14:51 +0800

    style: 格式修改

--- src/apis/home/getHotTagsApi.js
+++ src/apis/home/getHotTagsApi.js
@@ -2,25 +2,22 @@ import { instance } from "@/server/request";
 
 // 引入cityStore
 import { useCityStore } from "@/store/useCityStore/useCityStore";
-const cityStore = useCityStore()
+const cityStore = useCityStore();
 
 // 引入houseStore
 import { useHouseDataStore } from "@/store/useHouseDataStore/useHouseDataStore";
 
-const houseDataStore = useHouseDataStore()
+const houseDataStore = useHouseDataStore();
 
 export async function getHotTagsApi() {
-
-    
   return await instance({
-    method:'post',
-    url:'/bnbapp-node-h5/h5/portal/getportalconfig/bnb/v2',
-    data:{
-        checkInDate: houseDataStore.moveInDate.format('YYYY-MM-DD'),
-        checkOutDate: houseDataStore.moveOutDate.format('YYYY-MM-DD'),
-        cityId:cityStore.cityInfo.cityId,
-        feLog:{h5_base_log:""}
-    }
+    method: "post",
+    url: "/bnbapp-node-h5/h5/portal/getportalconfig/bnb/v2",
+    data: {
+      checkInDate: houseDataStore.moveInDate.format("YYYY-MM-DD"),
+      checkOutDate: houseDataStore.moveOutDate.format("YYYY-MM-DD"),
+      cityId: cityStore.cityInfo.cityId,
+      feLog: { h5_base_log: "" },
+    },
   });
-  
 }
