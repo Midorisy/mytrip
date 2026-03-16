0c3c56679072b83a2932d9c22e260558ff282169
Midorisy <837239572@qq.com>
2026-03-16 22:32:48 +0800

    feat: 首页新增全局加载模块

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 // TabBar底栏
 import TabBar from '@/components/Common/TabBar/index.vue'
+import LoadingPop from '@/components/Common/LoadingPop/index.vue'
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
@@ -8,6 +9,7 @@ const route = useRoute()
 
 <template>
   <div class="main">
+    <loading-pop></loading-pop>
     <router-view></router-view>
     <!-- 底部导航栏 -->
     <tab-bar class="tabbar" v-if="!route.meta.hideTabbar"></tab-bar>
