50706fa5f94a38b652f6e9f4ea8b15b083af78b3
Midorisy <837239572@qq.com>
2026-03-19 22:14:24 +0800

    feat: 新增顶层tabbar模块

---
+++ src/components/houseInfo/TopBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="top-bar">
+        <NavBar left-arrow left-text="黑马" @click-left="onClickLeft" title="房屋详情" >
+            <template #right>
+                <img class="right-menu" src="@/assets/img/home/icon-right-menu.png" alt="" srcset="">
+            </template>
+
+        </NavBar>
+    </div>
+</template>
+
+<script setup>
+import { NavBar } from 'vant';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+
+function onClickLeft() {
+    router.back()
+}
+
+</script>
+
+<style scoped>
+.top-bar{
+    --van-nav-bar-height:44px;
+    --van-nav-bar-arrow-size:22px;
+    --van-nav-bar-icon-color:var(--primary-color);
+    --van-nav-bar-text-color:var(--primary-color);
+
+    /* 样式穿透部分 */
+    &:deep(.van-nav-bar){
+        .van-nav-bar__title{
+            font-weight: 400;
+            font-size: 15px;
+        }
+    }
+
+
+    /* 图片大小 */
+    .right-menu{
+        width: 20px;
+        height: 16px;
+    }
+}
+
+</style>
